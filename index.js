da0be52cce5159dd12966f47930479a81e3f9ae1
Omid Kashan <omid@antilong.com>
2022-05-15 21:56:24 +0100

    initial commit from cloned repo

---
+++ index.js
@@ -0,0 +1,91 @@
+/**
+ * @typedef {import('mdast').Root} Root
+ * @typedef {import('mdast').Paragraph} Paragraph
+ * @typedef {import('mdast').Link} Link
+ * @typedef {import('mdast').LinkReference} LinkReference
+ */
+
+import {visit, SKIP} from 'unist-util-visit'
+import {whitespace} from 'hast-util-whitespace'
+
+const unknown = 1
+const containsImage = 2
+const containsOther = 3
+
+/**
+ * Plugin to remove the wrapping paragraph for images.
+ *
+ * @type {import('unified').Plugin<void[], Root>}
+ */
+export default function remarkUnwrapTags() {
+  return (tree) => {
+    visit(tree, 'paragraph', (node, index, parent) => {
+      console.log(parent)
+      if(node.children) {
+        node.children.forEach((child) => {
+          if(child.value) {
+            child.value = child.value.replaceAll("\n", '').replaceAll("\r"," ").toString()
+          }
+        })
+      }
+      //console.log(node.children)
+      if(node.children[0].value ==='(If you have a complete sentence that stands alone in brackets, start it with a capital letter and end it with a full stop before the closing bracket.)') {
+        console.log(parent)
+      }
+      if(parent.name==='p') {
+        parent.children = node.children
+      }
+      if (
+        parent &&
+        typeof index === 'number' &&
+        applicable(node) === containsImage
+      ) {
+        //console.log(parent.children)
+        //parent.children.splice(index, 1, ...node.children)
+        // return [SKIP, index]
+      }
+    })
+  }
+}
+
+/**
+ * @param {Paragraph|Link|LinkReference} node
+ * @param {boolean} [inLink]
+ * @returns {1|2|3}
+ */
+function applicable(node, inLink) {
+  /** @type {1|2|3} */
+  let image = unknown
+  let index = -1
+
+  while (++index < node.children.length) {
+    const child = node.children[index]
+
+    if (whitespace(child)) {
+      // White space is fine.
+    } else if (child.type === 'image' || child.type==='text' || child.type === 'imageReference') {
+      image = containsImage
+      if(child.type==='text') {
+        child.value = child.value.replace("\n", '').toString()
+      }
+
+    } else if (
+      !inLink &&
+      (child.type === 'link' || child.type === 'linkReference')
+    ) {
+      const linkResult = applicable(child, true)
+
+      if (linkResult === containsOther) {
+        return containsOther
+      }
+
+      if (linkResult === containsImage) {
+        image = containsImage
+      }
+    } else {
+      return containsOther
+    }
+  }
+
+  return image
+}
